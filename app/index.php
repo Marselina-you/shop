dbf956dba247fa33b21f82434c42be303254f54b
unknown <marselina88@list.ru>
2021-11-02 17:20:29 +0300

    2.11

---
+++ app/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+ 
+	<meta charset="UTF-8">
+	<title>Document</title>
+ 
+	<!-- Подключаем стили проекта -->
+	<link rel="stylesheet" href="css/app.min.css">
+ 
+</head>
+ 
+<body>
+ <?php
+ 	echo 'kuku';
+ ?>
+	<p>Далеко-далеко  словесными  в стране, гласных и согласных...</p>
+ 
+	<!-- Подключаем оптимизированное изображение -->
+	<img src="images/dest/image.jpg" alt="Alt">
+ 
+	<!-- Подключаем скрипты проекта -->
+	<script src="js/app.min.js"></script>
+ 
+</body>
+</html>
